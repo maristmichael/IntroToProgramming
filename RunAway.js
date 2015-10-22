69d14079ade663672294c9e36324d249eb66e6dc
Michael Gutierrez <michael.gutierrez2@marist.edu>
2015-10-21 20:03:00 -0400

    Added variables to keep track if player has visited area. Removed points system to start from scratch. Broke down long lines of code.

--- RunAway.js
+++ RunAway.js
@@ -1,13 +1,22 @@
+// Michael Gutierrez
 var currentRoom = "startingRoom";
 var currentPoints = 0;
-var pointsEarned = "Points: " + currentPoints;
-var points;
 
-function showScene(descrip) {
-    document.getElementById("scene").innerHTML = descrip;
+// The variables below keep track if a room was visited.
+
+var safeRoomVisit = false;
+var ratHallVisit = false;
+var redRoomVisit = false; 
+var signHallVisit = false; 
+var snakeRoomVisit = false;
+var falseSafeVisit = false;
+var freedomHallVisit = false;
+
+function showScene(message) {
+    document.getElementById("scene").innerHTML = message;
 }
-function showPoints(descrip) {
-    document.getElementById("points").innerHTML = descrip;
+function showPoints(message) {
+    document.getElementById("points").innerHTML = message;
 }
 
 function moveNorth() {
@@ -17,54 +26,46 @@ function moveNorth() {
     if (currentRoom === "startingRoom") {
         currentRoom = "thePitt";
         notice = "You somehow fell into the pitt that is in clear sight.<br>You are dead.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "thePitt") {
         notice = "You are still dead.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "safeRoom") {
         currentRoom = "ratHall";
         notice = "This is a long hallway, with statues of giant rats.<br>The pitt is to the east.";
-        currentPoints = 10;
-        points = "Points: " + currentPoints;
+        ratHallVisit = true;
+        points = "Points: "
     } else if (currentRoom === "ratHall") {
         currentRoom = "redMarkedRoom";
-        notice = "This room has two directions you can take.<br> There appears to be red marks all over the the path toward the west path.<br>West or East, choose wisely...";
-        currentPoints = 15;
-        points = "Points: " + currentPoints;
+        notice = "This room has two directions you can take.<br>" + 
+                 "There appears to be red marks all over the the path toward the west path.<br>" + 
+                 "West or East, choose wisely...";
+        points = "Points: " 
+        redRoomVisit = true;
     } else if (currentRoom === "freedomHallway") {
         currentRoom = "giantSnakeRoom";
         notice = "You went back into the room with the Giant Snake.<br>Great decision...";
-        currentPoints = 25;
-        points = "Points: " + currentPoints;
+        points = "Points: "
     } else if (currentRoom === "giantSnakeRoom") {
         currentRoom = "choseToDieRoom";
         notice = "For some reason you decided to wake the snake.<br>It goes without saying, you were eaten...";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "choseToDieRoom") {
         notice = "You were devoured.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "deadEnd") {
         notice = "You have been eaten.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "trapRoom") {
         notice = "You were devoured.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "freedom") {
         notice = "You've escaped this wretched place. No more snakes!<br>FREEDOM!";
-        currentPoints = currentPoints;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
     } else {
         notice = "You bumped into a wall, and fell hilariously.<br>Choose a different direction.";
-        currentPoints = currentPoints;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
     }
-    
     showScene(notice);
     showPoints(points);
 }
@@ -75,58 +76,48 @@ function moveWest() {
     
     if (currentRoom === "startingRoom") {
         currentRoom = "safeRoom";
-        notice = "You escaped to a safe room.<br>There are no snakes in the room, but the snakes will follow you if you dawdle for too long.";
-        currentPoints = 5;
-        points = "Points: " + currentPoints;
+        notice = "You escaped to a safe room.<br>" + 
+                 "There are no snakes in the room, but the snakes will follow you if you dawdle for too long.";
+        points = "Points: " 
+        safeRoomVisit = true
     } else if (currentRoom === "redMarkedRoom") {
         currentRoom = "deadEnd";
         notice = "Dead end...<br>The snakes caught up to you and had you for dinnner.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "deadEnd") {
         notice = "You have been eaten.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "thePitt") {
         notice = "You are still dead.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "signHall") {
         currentRoom = "redMarkedRoom";
         notice = "You are back in the room with red marks toward the west.";
-        currentPoints = 15;
-        points = "Points: " + currentPoints;
+        points = "Points: "
     } else if (currentRoom === "giantSnakeRoom") {
         currentRoom = "signHall";
         notice = "This is the room with the sign.<br>Are you going to follow the sign south?";
-        currentPoints = 20;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
     } else if (currentRoom === "falseSafeRoom") {
         currentRoom = "giantSnakeRoom";
         notice = "You are back in the room with the giant snake.<br>Don't disturb it!";
-        currentPoints = 25;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
     } else if (currentRoom === "trapRoom") {
         notice = "You were devoured.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "freedomHallway") {
         currentRoom = "thePitt";
         notice = "You somehow fell into the pitt that is in clear sight.<br>You are dead.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "choseToDieRoom") {
         notice = "You were devoured.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "freedom") {
         notice = "You've escaped this wretched place. No more snakes!<br>FREEDOM!";
-        currentPoints = currentPoints;
-        points = "Points: " + currentPoints;
+        points = "Points: "
     } else {
         notice = "You bumped into a wall, and fell hilariously.<br>Choose a different direction.";
-        currentPoints = currentPoints;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
     }
     showScene(notice);
     showPoints(points);
@@ -139,57 +130,51 @@ function moveEast() {
     if (currentRoom === "safeRoom") {
         currentRoom = "startingRoom";
         notice = "There are snakes in here you idiot. GET OUT!";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
+        safeRoomVisit = true;
     } else if (currentRoom === "ratHall") {
         currentRoom = "thePitt";
         notice = "You somehow fell into the pitt that is in clear sight.<br>You are dead.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "thePitt") {
         notice = "You are still dead.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "deadEnd") {
         notice = "You have been eaten.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "redMarkedRoom") {
         currentRoom = "signHall";
         notice = "This room has a sign that states 'Freedom is South.'<br> Seems legit...";
-        currentPoints = 20;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
+        signHallVisit = true;
     } else if (currentRoom === "signHall") {
         currentRoom = "giantSnakeRoom";
-        notice = "THERE IS A 30 FOOT SNAKE NEAR THE NORTH WALL FACING EASTWARD. DONT DISTURB IT!<br>You can escape by heading East or South.";
-        currentPoints = 25;
-        points = "Points: " + currentPoints;
+        notice = "THERE IS A 30 FOOT SNAKE NEAR THE NORTH WALL FACING EASTWARD. DONT DISTURB IT!<br>" + 
+                 "You can escape by heading East or South.";
+        points = "Points: " 
+        snakeRoomVisit = true;
     } else if (currentRoom === "giantSnakeRoom") {
         currentRoom = "falseSafeRoom";
         notice = "You escaped that huge snake, atleast for now.<br>Continue onwards.";
-        currentPoints = 30;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
+        falseSafeVisit = true;
     } else if (currentRoom === "falseSafeRoom") {
         currentRoom = "trapRoom";
-        notice = "There's no where to go...<br> The giant snake slithered to your location and ate you whole with its massive mouth.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        notice = "There's no where to go...<br>" + 
+                 "The giant snake slithered to your location and ate you whole with its massive mouth.";
+        points = "Points: " + 0;
     } else if (currentRoom === "trapRoom") {
         notice = "You were devoured.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "choseToDieRoom") {
         notice = "You were devoured.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "freedom") {
         notice = "You've escaped this wretched place. No more snakes!<br>FREEDOM!";
-        currentPoints = currentPoints;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
     } else {
         notice = "You bumped into a wall, and fell hilariously.<br>Choose a different direction.";
-        currentPoints = currentPoints;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
     }
     showScene(notice);
     showPoints(points);
@@ -202,53 +187,45 @@ function moveSouth() {
     if (currentRoom === "ratHall") {
         currentRoom = "safeRoom";
         notice = "You are back in the safe room. You have to leave this place eventually...";
-        currentPoints = 5;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
     } else if (currentRoom === "redMarkedRoom") {
         currentRoom = "ratHall";
         notice = "This is the room with the rat statues and pitt to the east.";
-        currentPoints = 10;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
     } else if (currentRoom === "signHall") {
         currentRoom = "thePitt";
         notice = "It was not freedom...<br>You walked into the pitt which means you are dead.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "thePitt") {
         notice = "You are still dead.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "deadEnd") {
         notice = "You have been eaten.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "trapRoom") {
         notice = "You were devoured.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "choseToDieRoom") {
         notice = "You were devoured.";
-        currentPoints = 0;
-        points = "Points: " + currentPoints;
+        points = "Points: " + 0;
     } else if (currentRoom === "giantSnakeRoom") {
         currentRoom = "freedomHallway";
-        notice = "You entered another room with statues of giant rats, and the pitt is to the west.<br> This time however, there appears to be greenerie in the ground. That is a good sign.";
-        currentPoints = 30;
-        points = "Points: " + currentPoints;
+        notice = "You entered another room with statues of giant rats, and the pitt is to the west.<br>" +
+                 "This time however, there appears to be greenerie in the ground. That is a good sign.";
+        points = "Points: " 
     } else if (currentRoom === "freedomHallway") {
         currentRoom = "freedom";
         notice = "You've escaped this wretched place. No more snakes!<br>FREEDOM!";
-        currentPoints = 50;
-        points = "Points: " + currentPoints;
+        points = "Points: "
+        freedomHallVisit = true;
     } else if (currentRoom === "freedom") {
         notice = "You've escaped this wretched place. No more snakes!<br>FREEDOM!";
-        currentPoints = currentPoints;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
     } else {
         notice = "You bumped into a wall, and fell hilariously.<br>Choose a different direction.";
-        currentPoints = currentPoints;
-        points = "Points: " + currentPoints;
+        points = "Points: " 
     }
     showScene(notice);
     showPoints(points);
 }
+
