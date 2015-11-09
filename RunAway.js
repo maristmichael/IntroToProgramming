9f509b84a2e28b51de0b69e8309291e431518b15
Michael Gutierrez <michael.gutierrez2@marist.edu>
2015-11-08 20:12:15 -0500

    Corrected project 3: Added block comments, fixed type-to-move functions, added spaces for readability.

--- RunAway.js
+++ RunAway.js
@@ -1,4 +1,10 @@
-// Michael Gutierrez
+/* Michael Gutierrez
+ * Matthew Johnson
+ * CMPT 120L-115
+ * November 8, 2015
+ * Project 3
+ */
+
 
 var currentRoom = "startingRoom";
 var currentPoints = 0;
@@ -7,6 +13,7 @@ var currentPoints = 0;
 function pointCount() {
 	return "Points: " + currentPoints;
 }
+
 function zeroPoints() {
 	return "Points: 0";
 }
@@ -24,8 +31,13 @@ var freedomHallVisit = false;
 function showScene(message) {
     document.getElementById("message").innerHTML = message;
 }
+
 function showPoints(message) {
     document.getElementById("points").innerHTML = message;
+}
+
+function showInvalidDirection(error) {
+	document.getElementById("invalidDirection").innerHTML = error;
 }
 
 // The functions below disable/enable html buttons.
@@ -42,6 +54,7 @@ function disableButton(button){
 		document.getElementById("goButton").disabled = true;
 	}
 }
+
 function enableButton(button){
 	if (button === "north") {
 		document.getElementById("northButton").disabled = false;
@@ -55,6 +68,7 @@ function enableButton(button){
 		document.getElementById("goButton").disabled = false;
 	}
 }
+
 function disableAllButtons(){
 	disableButton("north");
 	disableButton("west");
@@ -62,6 +76,7 @@ function disableAllButtons(){
 	disableButton("south");
 	disableButton("go");
 }
+
 function enableAllButtons(){
 	enableButton("north");
 	enableButton("west");
@@ -72,34 +87,37 @@ function enableAllButtons(){
 
 // The function below handels the text input.
 function textInput(userInput) {
+	var error;
 	var userInput = document.getElementById("command").value;
+	
 	if (userInput === "N") {
-		alert("You entered " + userInput);
 		moveNorth();
+		error = "";
 	} else if (userInput === "n") {
-		alert("You entered " + userInput);
 		moveNorth();
+		error = "";
 	} else if (userInput === "S") {
-		alert("You entered " + userInput);
 		moveSouth();
+		error = "";
 	} else if (userInput === "s") {
-		alert("You entered " + userInput);
 		moveSouth();
+		error = "";
 	} else if (userInput === "E") {
-		alert("You entered " + userInput);
 		moveEast();
+		error = "";
 	} else if (userInput === "e") {
-		alert("You entered " + userInput);
 		moveEast();
+		error = "";
 	} else if (userInput === "W") {
-		alert("You entered " + userInput);
 		moveWest();
+		error = "";
 	} else if (userInput === "w") {
-		alert("You entered " + userInput);
 		moveWest();
+		error = "";
 	} else {
-		alert("You entered an incorrect direction. Try again.");
+		error = "You entered an invalid direction. Try again.";
 	}
+	showInvalidDirection(error);
 }
 
 // The functions below are location and scenerio descriptions.
@@ -108,90 +126,105 @@ function backToStart() {
     message = "There are snakes in here you idiot. GET OUT!";
     return message;
 }
+
 function safeRoom() {
     var message;
     message = "You are in a safe room.<br>" +
 "There are no snakes in the room, but the snakes will follow you if you dawdle for too long.";
     return message;
 }
+
 function backToSafeRoom(){
     var message;
     message = "You are back in the safe room.<br>" + 
 "You have to leave this place eventually...";
     return message;
 }
+
 function ratHall(){
     var message;
     message = "This is a long hallway, with statues of giant rats.<br>" +
 "The pitt is to the east.";
     return message;
 }
+
 function backToRatHall(){
     var message;
     message = "This is the room with the rat statues and pitt to the east.";
     return message;
 }
+
 function redMarkedRoom(){
     var message;
     message = "This room has two directions you can take.<br>" + 
 "There appears to be red marks all over the the path toward the west path.<br>" + "West or East, choose wisely...";
     return message;
 }
+
 function backToRedRoom(){
     var message;
     message = "You are back in the room with red marks toward the west.";
     return message;
 }
+
 function deadEnd(){
     var message;
     message = "Dead end...<br>" + 
 "The snakes caught up to you and had you for dinnner.";
     return message;
 }
+
 function signHall(){
     var message;
     message = "This room has a sign that states 'Freedom is South.'<br>" + "Seems legit...";
     return message;
-    
 }
+
 function signDeath(){
     var message;
     message = "It was not freedom...<br>" + 
 "You walked into the pitt which means you are dead.";
     return message;
 }
+
 function backToSignHall(){
     var message;
     message = "This is the room with the sign.<br>Are you going to follow the sign south?";
     return message;
 }
+
 function giantSnakeRoom(){
     var message;
     message = "THERE IS A 30 FOOT SNAKE NEAR THE NORTH WALL FACING EASTWARD.<br>" + "DONT DISTURB IT!<br>" + "You can escape by heading East or South.";
     return message;
 }
+
 function backToGiantSnake(){
     var message;
     message = "You went back into the room with the Giant Snake.<br>" + "Great decision...<br>" + "South or East...";
     return message;
 }
+
 function choseToDieRoom(){
     var message;
     message = "For some reason you decided to wake the snake.<br>" + 
 "It goes without saying, you were eaten...";
     return message;
 }
+
 function falseSafeRoom(){
     var message;
     message = "You escaped that huge snake, atleast for now.<br>Continue onwards.";
     return message;
 }
+
 function trapRoom(){
     var message;
     message = "There's no where to go...<br>" + 
 "The giant snake slithered to your location and ate you whole with its massive mouth.";
     return message;
 }
+
 function freedomHallway(){
     var message;
     message = "You entered another room with statues of giant rats, and the pitt is to the west.<br>" +
@@ -199,31 +232,37 @@ function freedomHallway(){
 "That is a good sign.";
     return message;
 }
+
 function freedom(){
     var message;
     message = "You've escaped this wretched place. No more snakes!<br> FREEDOM!";
     return message;
 }
+
 function dumbDeath(){
     var message;
     message = "You somehow fell into the pitt that is in clear sight.<br>You are dead.";
     return message;
 }
+
 function stillAreDead(){
     var message;
     message = "You are still dead."
     return message;
 }
+
 function youreDevoured(){
     var message;
     message = "You were devoured";
     return message;
 }
+
 function yourEaten(){
     var message;
     message = "You have been eaten."
     return message;
 }
+
 function hitWall(){
     var message;
     message = "You bumped into a wall, and fell hilariously.<br>" +
@@ -239,6 +278,7 @@ function pointsSafeRoom(){
 		return currentPoints;
 	}
 }
+
 function pointsRatHall(){
 	if (ratHallVisit === false) {
         currentPoints += 5;
@@ -246,6 +286,7 @@ function pointsRatHall(){
 		return currentPoints;
 	}
 }
+
 function pointsRedRoom(){
 	if (redRoomVisit === false) {
         currentPoints += 5;
@@ -253,6 +294,7 @@ function pointsRedRoom(){
 		return currentPoints;
 	}
 }
+
 function pointsSignHall(){
 	if (signHallVisit === false) {
         currentPoints += 5;
@@ -260,6 +302,7 @@ function pointsSignHall(){
 		return currentPoints;
 	}
 }
+
 function pointsSnakeRoom(){
 	if (snakeRoomVisit === false) {
         currentPoints += 5;
@@ -267,6 +310,7 @@ function pointsSnakeRoom(){
 		return currentPoints;
 	}
 }
+
 function pointsFalseSafe(){
 	if (falseSafeVisit === false) {
         currentPoints += 5;
@@ -274,6 +318,7 @@ function pointsFalseSafe(){
 		return currentPoints;
 	}
 }
+
 function pointsFreedomHall(){
 	if (freedomHallVisit === false) {
         currentPoints += 5;
@@ -343,6 +388,7 @@ function moveNorth() {
     showScene(message);
     showPoints(points);
 }
+
 function moveWest() {
     var message;
     var points;
@@ -406,6 +452,7 @@ function moveWest() {
     showScene(message);
     showPoints(points);
 }
+
 function moveEast() {
     var message;
     var points;
@@ -473,6 +520,7 @@ function moveEast() {
     showScene(message);
     showPoints(points);
 }
+
 function moveSouth() {
     var message;
     var points;
